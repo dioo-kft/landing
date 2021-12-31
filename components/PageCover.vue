a2d3995cc8241caf19dd0e62b4ef2faa5c230be5
Konkolyi Szilvia <sziszikonkolyi@gmail.com>
2021-12-31 13:46:38 +0100

    add pledge to cover

--- components/PageCover.vue
+++ components/PageCover.vue
@@ -1,30 +1,30 @@
 <template>
     <b-row class="page-cover my-5 py-5" align-v="center" align-h="center">
-        <b-col class="page-cover-img text-right" sm="auto">
-            <img src="~/assets/img/tunder.png" />
-        </b-col>
-        <b-col align-self="stretch" sm="auto">
-            <b-row>
-                <b-col align-self="center">
-                    <b-row align-h="center" class="page-cover-title">
-                        <b-col sm="auto">
-                            <img src="~/assets/img/DIOO_name.png" />
-                        </b-col>
-                    </b-row>
-                    <b-row align-h="center" class="page-cover-subtitle">
-                        <b-col sm="auto">Digitális OkosJáték Óvodásoknak</b-col>
-                    </b-row>
-                    <b-row  align-h="center">
-                        <b-col sm="auto">
-                            <b-button pill class="button-outline-brand-color" href="#contact">Üzenjen nekünk</b-button>
-                        </b-col>
-                    </b-row>
-                    <!-- <p class="page-cover-title">DIOO</p>
-                    <p class="page-cover-subtitle">Digitális OkosJáték Óvodásoknak</p>
+                <b-col class="page-cover-img text-right" sm="auto">
+                    <img src="~/assets/img/tunder.png" />
+                </b-col>
+                <b-col align-self="stretch" sm="auto">
+                    <b-row>
+                        <b-col align-self="center">
+                            <b-row align-h="center" class="page-cover-title">
+                                <b-col sm="auto">
+                                    <img src="~/assets/img/DIOO_name.png" />
+                                </b-col>
+                            </b-row>
+                            <b-row align-h="center" class="page-cover-subtitle">
+                                <b-col sm="auto">Digitális OkosJáték Óvodásoknak</b-col>
+                            </b-row>
+                            <b-row  align-h="center">
+                                <b-col sm="auto">
+                                    <b-button pill class="button-outline-brand-color" href="#contact">Üzenjen nekünk</b-button>
+                                </b-col>
+                            </b-row>
+                            <!-- <p class="page-cover-title">DIOO</p>
+                            <p class="page-cover-subtitle">Digitális OkosJáték Óvodásoknak</p>
 
-                    <b-button pill class="button-outline-brand-color">Üzenjen nekünk</b-button> -->
+                            <b-button pill class="button-outline-brand-color">Üzenjen nekünk</b-button> -->
+                        </b-col>
+                    </b-row>
                 </b-col>
             </b-row>
-        </b-col>
-    </b-row>
 </template>
