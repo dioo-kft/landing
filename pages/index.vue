6027a657eedd872a73f0dae0a80ae6966a2e900d
Konkolyi Szilvia <sziszikonkolyi@gmail.com>
2022-01-02 15:51:28 +0100

    Revert "add color styler for designing"
    This reverts commit 8e8db3e38852cb20c876dcc75682d1621542a6d4.

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,6 @@
 <template>
-  <div :style="userStyle">
+  <div>
     <navigation-bar />
-    
-    <b-container class="color-picker">
-      <b-row>
-        <b-col>
-          <b-form-group label="Márkaszín" label-for="input-1">
-            <b-form-input id="input-1" v-model="form.brandColor" type="color" />
-          </b-form-group>
-        </b-col>
-        <b-col>
-          <b-form-group label="Világos szín 1" label-for="input-2">
-            <b-form-input id="input-2" v-model="form.lightBg" type="color" />
-          </b-form-group>
-        </b-col>
-        <b-col>
-          <b-form-group label="Világos szín 2" label-for="input-3">
-            <b-form-input id="input-3" v-model="form.lightAcc" type="color" />
-          </b-form-group>
-        </b-col>
-        <b-col>
-          <b-form-group label="Sötét szín" label-for="input-4">
-            <b-form-input id="input-4" v-model="form.darkBg" type="color" />
-          </b-form-group>
-        </b-col>
-        <b-col>
-          <b-form-group label="Kiemelő szín" label-for="input-5">
-            <b-form-input id="input-5" v-model="form.darkAcc" type="color" />
-          </b-form-group>
-        </b-col>
-      </b-row>
-    </b-container>
 
     <b-container fluid>
         <b-overlay show="true" opacity="0">
@@ -56,34 +26,7 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        form: {
-          brandColor: '#FDD327',
-          lightBg: '#F4F6F5',
-          lightAcc: '#9DAABB',
-          darkBg: '#233245',
-          darkAcc: '#B32737',
-        },
-      }
-    },
-    computed: {
-      userStyle () {
-        return {
-          '--brand-color': this.form.brandColor,
-          '--light-bg-color': this.form.lightBg,
-          '--light-accent-color': this.form.lightAcc,
-          '--dark-accent-color': this.form.darkAcc,
-          '--dark-bg-color': this.form.darkBg,
-        }
-      }
-    },
-  }
+export default {
+  name: 'IndexPage'
+}
 </script>
-
-<style>
-/* :root {
-  --brand-color: var(--brand-color),
-} */
-</style>
