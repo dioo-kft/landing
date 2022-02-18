59363079270f9c388b766c7f333dcd3681906cb8
Szilvia Konkolyi <sziszikonkolyi@gmail.com>
2022-02-18 18:26:50 +0100

    add new forms to contact us

--- components/PageContact.vue
+++ components/PageContact.vue
@@ -1,39 +1,83 @@
 <template>
-    <div class="page-contact">
-        <b-row align-h="center">
-            <b-col md="5" class="page-contact-col-bg mx-3 p-5 text-center my-3 my-md-0">
-                <b-row cols="1" align-content="around" class="h-100">
-                    <b-col>
-                        <b-button pill class="button-brand-color" href="mailto:info@dioo.hu?subject=DIOO%20|%20Érdeklődés">Üzenjen nekünk</b-button>
-                    </b-col>
-                    <b-col>
-                        <b-button pill class="button-light-inverted" href="mailto:info@dioo.hu?subject=DIOO%20|%20Bemutatót%20kérek">Bemutatót kérek</b-button>
-                    </b-col>
-                </b-row>
-            </b-col>
-            <b-col md="5" class="page-contact-col-bg p-5 mx-3 my-3 my-md-0">
-                <b-row cols="1" align-h="center">
-                    <b-col sm="auto">
-                        <h4 class="text-center contact-subtitle">Keressen minket!</h4>
-                        <p>email címünk: <a href="mailto:info@dioo.hu">&#105;&#110;&#102;&#111;&#064;&#100;&#105;&#111;&#111;&#046;&#104;&#117;</a></p>
-                        <h5>Fenntartóknak, Pedagógusoknak</h5>
-                        <p>Kapcsolattartó: Konkolyi Sándor</p>
-                        <p style="margin-bottom: 0; padding-bottom:0;">tel.: +&#051;&#054;&#032;&#051;&#048;&#032;&#056;&#055;&#057;&#032;&#056;&#055;&#056;&#053;</p>
-                        <p style="font-size: 12px;">Hétköznaponként 8:00 - 17:00 között hívható.</p>
-                    </b-col>
-                    <b-col sm="auto">
-                        <h5>Társadalmi Felelősségvállalás, Vállalatoknak</h5>
-                        <p>Kapcsolattartó: Konkolyi Szilvia</p>
-                        <p style="margin-bottom: 0; padding-bottom:0;">tel.: &plus;&#51;&#54;&#51;&#48;&#49;&#57;&#54;&#54;&#56;&#50;&#50;</p>
-                        <p style="font-size: 12px;">Hétköznaponként 8:00 - 17:00 között hívható.</p>
-                    </b-col>
-                    <b-col class="text-center pt-3">
-                        <b-link  href="https://www.facebook.com/diootanito">
-                            <b-icon class="contact-facebook-icon" icon="facebook"></b-icon>
-                        </b-link>
-                    </b-col>
-                </b-row>
-            </b-col>
+  <div class="page-contact">
+    <b-row align-h="center">
+      <b-col
+        md="5"
+        class="page-contact-col-bg mx-3 p-5 text-center my-3 my-md-0"
+      >
+        <b-row align-content="between" align-v="center" class="h-100 justify-content-center" >
+          <b-col cols="auto">
+            <b-button
+              pill
+              class="button-light-inverted"
+              href="https://forms.clickup.com/f/4bd9e-3121/C0VWX3U1BMFPYUQKSF"
+              >Bemutatót kérek</b-button
+            >
+          </b-col>
+          <b-col cols="auto"
+            ><b-button
+              pill
+              class="button-light-inverted"
+              href="https://forms.clickup.com/f/4bd9e-3001/ZXRM9YPGZ98O8YC29Y"
+              >DIOO-t veszek</b-button
+            ></b-col
+          >
+          <b-col cols="auto"
+            ><b-button
+              pill
+              class="button-light-inverted"
+              href="https://forms.clickup.com/f/4bd9e-3081/UU6XGDB170WL17YDZK"
+              >Szülői érdeklődés</b-button
+            ></b-col
+          >
+          <b-col cols="auto"
+            ><b-button
+              pill
+              class="button-light-inverted"
+              href="https://forms.clickup.com/f/4bd9e-3041/NO44ISE6HP07EN8GGX"
+              >Támogatok egy óvodát</b-button
+            ></b-col
+          >
         </b-row>
-    </div>
+      </b-col>
+      <b-col md="5" class="page-contact-col-bg p-5 mx-3 my-3 my-md-0">
+        <b-row cols="1" align-h="center">
+          <b-col sm="auto">
+            <h4 class="text-center contact-subtitle">Keressen minket!</h4>
+            <p>
+              email címünk:
+              <a href="mailto:info@dioo.hu"
+                >&#105;&#110;&#102;&#111;&#064;&#100;&#105;&#111;&#111;&#046;&#104;&#117;</a
+              >
+            </p>
+            <h5>Fenntartóknak, Pedagógusoknak</h5>
+            <p>Kapcsolattartó: Konkolyi Sándor</p>
+            <p style="margin-bottom: 0; padding-bottom: 0">
+              tel.:
+              +&#051;&#054;&#032;&#051;&#048;&#032;&#056;&#055;&#057;&#032;&#056;&#055;&#056;&#053;
+            </p>
+            <p style="font-size: 12px">
+              Hétköznaponként 8:00 - 17:00 között hívható.
+            </p>
+          </b-col>
+          <b-col sm="auto">
+            <h5>Társadalmi Felelősségvállalás, Vállalatoknak</h5>
+            <p>Kapcsolattartó: Konkolyi Szilvia</p>
+            <p style="margin-bottom: 0; padding-bottom: 0">
+              tel.:
+              &plus;&#51;&#54;&#51;&#48;&#49;&#57;&#54;&#54;&#56;&#50;&#50;
+            </p>
+            <p style="font-size: 12px">
+              Hétköznaponként 8:00 - 17:00 között hívható.
+            </p>
+          </b-col>
+          <b-col class="text-center pt-3">
+            <b-link href="https://www.facebook.com/diootanito">
+              <b-icon class="contact-facebook-icon" icon="facebook"></b-icon>
+            </b-link>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </div>
 </template>
